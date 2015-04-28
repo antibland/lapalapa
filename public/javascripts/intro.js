5e4e79ed341ad5eb5adf52c60675835fc2629c45
Andy Hoffman <antibland@gmail.com>
2015-04-28 00:27:36 -0400

    Evolve opening animation

--- public/javascripts/intro.js
+++ public/javascripts/intro.js
@@ -1,15 +1,27 @@
 var intro = (function() {
-  var step_one         = document.querySelector(".step.one"),
-      step_two         = document.querySelector(".step.two"),
-      animation_end    = utilities.whichAnimationEvent();
+  var step_one      = document.querySelector(".step.one"),
+      step_two      = document.querySelector(".step.two"),
+      step_three    = document.querySelectorAll(".step.three"),
+      step_four     = document.querySelector(".step.four"),
+      animation_end = utilities.whichAnimationEvent();
 
   function init() {
     step_one.classList.add("go");
   }
 
   if (animation_end) {
-    document.querySelector(".step").addEventListener(animation_end, function() {
+    step_one.addEventListener(animation_end, function() {
       step_two.classList.add("go");
+    }, false);
+
+    step_two.addEventListener(animation_end, function() {
+      [].forEach.call(step_three, function(step) {
+        step.classList.add("go");
+      });
+    }, false);
+
+    step_three[0].addEventListener(animation_end, function() {
+      step_four.classList.add("go");
     }, false);
   }
 
