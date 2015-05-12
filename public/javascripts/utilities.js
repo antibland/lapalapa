934bfacaccc038f7f105585c058dfef9eaf8674a
Andy Hoffman <antibland@gmail.com>
2015-05-12 16:49:33 -0400

    Add critical CSS for speed

--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -2,6 +2,17 @@ var utilities = (function() {
   "use strict";
 
   var ret = {
+    loadStyleSheet: function(src) {
+      if (document.createStyleSheet) {
+        document.createStyleSheet(src);
+      } else {
+        var stylesheet = document.createElement('link');
+        stylesheet.href = src;
+        stylesheet.rel = 'stylesheet';
+        stylesheet.type = 'text/css';
+        document.getElementsByTagName('head')[0].appendChild(stylesheet);
+      }
+    },
     getParameterByName: function(name) {
       name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
       var regex   = new RegExp("[\\?&]" + name + "=([^&#]*)"),
