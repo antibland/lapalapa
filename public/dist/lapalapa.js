7774a10b6ca028c877e5d7361d0b847a36e93e0a
Andy Hoffman <antibland@gmail.com>
2015-05-15 15:01:01 -0400

    Make active nav smoother

--- public/dist/lapalapa.js
+++ public/dist/lapalapa.js
@@ -314,9 +314,18 @@
   function bindings() {
     var nav_links = document.querySelectorAll('.nav-link');
 
+    function removeActiveNav() {
+      [].forEach.call(nav_links, function (el) {
+        el.classList.remove('active-nav');
+        el.classList.add('inactive-nav');
+      });
+    }
+
     [].forEach.call(nav_links, function (el) {
-      el.addEventListener('click', function() {
-        el.classList.add('active');
+      el.addEventListener('click', function(e) {
+        removeActiveNav();
+        el.classList.remove('inactive-nav');
+        el.classList.add('active-nav');
       }, false);
     });
 
